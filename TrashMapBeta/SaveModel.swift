41ba76dfddaf34b4b15675beeb06c6eae1ccc6ab
Daniel Inderwies <inderwies.daniel@web.de>
2019-08-05 23:53:11 +0200

    Dev of Beta of TrashMap

---
+++ TrashMapBeta/SaveModel.swift
@@ -0,0 +1,13 @@
+//
+//  SaveModel.swift
+//  TrashMapBeta
+//
+//  Created by Daniel Inderwies on 8/5/19.
+//  Copyright © 2019 Daniel Inderwies. All rights reserved.
+//
+
+import CoreData
+
+
+
+
