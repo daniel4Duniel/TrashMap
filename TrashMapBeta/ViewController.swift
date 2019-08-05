54104ea081bb24247457730bdfaf42b50d8d7981
Daniel Inderwies <inderwies.daniel@web.de>
2019-08-05 23:20:49 +0200

    Initial Commit

---
+++ TrashMapBeta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrashMapBeta
+//
+//  Created by Daniel Inderwies on 8/5/19.
+//  Copyright © 2019 Daniel Inderwies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
